304c31f1ec444903f96ba3ea5eff2665e080d727
Nagarajan V <nagarajan.v@sonata-software.com>
2021-03-23 12:41:55 +0530

    Api Committed by Nagaraj

--- evaluationapi/Startup.cs
+++ evaluationapi/Startup.cs
@@ -25,6 +25,7 @@ namespace evaluationapi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Added My Nagarajan
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
