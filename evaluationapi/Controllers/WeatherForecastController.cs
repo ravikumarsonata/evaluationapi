2a80dfdfc500d84a8926df19681e6bbacefb2bbe
wajid1234 <81307507+wajid1234@users.noreply.github.com>
2021-03-31 08:52:28 +0530

    Update WeatherForecastController.cs

--- evaluationapi/Controllers/WeatherForecastController.cs
+++ evaluationapi/Controllers/WeatherForecastController.cs
@@ -26,6 +26,7 @@ namespace evaluationapi.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            int i=0;
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
